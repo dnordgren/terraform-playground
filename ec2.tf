f7b70484ca47826a321f058f49881f706f922dc0
Derek Nordgren <derek.nordgren@hudl.com>
2020-10-13 16:04:56 -0500

    Set up input variables.

--- ec2.tf
+++ ec2.tf
@@ -9,11 +9,11 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region  = var.region
 }
 
 resource "aws_instance" "derek-example" {
-  ami           = "ami-0dba2cb6798deb6d8" # Ubuntu Server 20.04 LTS from us-east-1
+  ami           = var.amis[var.region]
   instance_type = "t2.micro"
   subnet_id     = "subnet-0d00e43d1dc20ff38" # Had to create this in AWS Console
 }
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+amis = {
+  "us-east-1" = "ami-0dba2cb6798deb6d8"
+  "us-west-2" = "ami-def456"
+}
