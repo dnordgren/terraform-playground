f8945ba1fa5794b5866913cb41c648e4fb30c394
Derek Nordgren <derek.nordgren@hudl.com>
2020-10-13 16:11:15 -0500

    Create sample for using output variables.
    `terraform output ip` after apply-time to view.

--- ec2.tf
+++ ec2.tf
@@ -12,8 +12,25 @@ provider "aws" {
   region  = var.region
 }
 
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "amis" {
+  type = map # see terraform.tfvars
+}
+
 resource "aws_instance" "derek-example" {
   ami           = var.amis[var.region]
   instance_type = "t2.micro"
   subnet_id     = "subnet-0d00e43d1dc20ff38" # Had to create this in AWS Console
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.derek-example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
 }
